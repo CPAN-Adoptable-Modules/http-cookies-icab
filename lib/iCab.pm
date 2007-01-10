490cf6bd5bcac8f371468eaa02431aaf8d8c9b82
brian d foy <brian.d.foy@gmail.com>
2007-01-10 05:04:31 +0000

    * updated for copyright and license
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1997 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/iCab.pm
+++ lib/iCab.pm
@@ -35,12 +35,12 @@ members of the project can shepherd this module appropriately.
 
 brian d foy, C<< <bdfoy@cpan.org> >>
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright 2003-2006, brian d foy, All rights reserved
+Copyright (c) 2003-2007 brian d foy.  All rights reserved.
 
-This library is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
 
